543a0378d5aaaa43527e70ea6af2f4931f372522
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-12-27 10:41:51 -0700

    Reformat

--- spongebobify.rkt
+++ spongebobify.rkt
@@ -2,6 +2,8 @@
 
 (provide spongebobify)
 
-(define (spongebobify text [start-lower #t]) (foldl (lambda (el index result)
-                                        (string-append result (string (if (xor start-lower (= 0 (modulo index 2))) (char-upcase el) (char-downcase el)))))
-                                "" (string->list text) (stream->list (in-range (string-length text)))))
+(define (spongebobify text [start-lower #t])
+  (foldl (lambda (el index result)
+                 (string-append result
+                                (string (if (xor start-lower (= 0 (modulo index 2))) (char-upcase el) (char-downcase el)))))
+         "" (string->list text) (stream->list (in-range (string-length text)))))
