e53be28ecf313cb2ea89d8c4c7e16dbd390c085f
Yunyu Lin <yl@vanderbilt.edu>
2018-02-24 09:46:14 -0600

    Slight optimization

--- spongebobify.rkt
+++ spongebobify.rkt
@@ -3,7 +3,6 @@
 (provide spongebobify)
 
 (define (spongebobify text [start-lower #t])
-  (foldl (lambda (el index result)
-                 (string-append result
-                                (string (if (xor start-lower (= 0 (modulo index 2))) (char-upcase el) (char-downcase el)))))
-         "" (string->list text) (stream->list (in-range (string-length text)))))
+  (list->string (map (lambda (el index)
+                       ((if (xor start-lower (= 0 (modulo index 2))) char-upcase char-downcase) el))
+                     (string->list text) (range (string-length text)))))
